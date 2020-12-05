68b2f96836768d78fc796feb2b6c901dbd91fabc
Vickoman <vicmandlagasca@gmail.com>
2020-12-05 16:24:31 -0500

    Add dotenv and configure apolloClient through ENV VAR

--- src/Components/Lista.jsx
+++ src/Components/Lista.jsx
@@ -1,14 +1,23 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 
-export class Lista extends React.Component {
-    constructor(props) {
-        super(props);
-        return;
+const QUERY_LIST = gql`
+    query QUERY_LIST{
+        lists {
+            id
+            name
+        }
     }
-
-    render() {
-        return (
-            <div></div>
-        );
-    }
+`;
+export function Lista() {
+    const {loading, error, data} = useQuery(QUERY_LIST);
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>{error.message}</p>
+    return (
+        <div>
+            {data.lists.map((list) => (
+                <div key={list.id}>{list.name}</div>
+            ))}
+        </div>
+    );
 }
