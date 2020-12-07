7666eb29808f68f11e4467c812d51d650f036de2
Vickoman <vicmandlagasca@gmail.com>
2020-12-06 22:33:59 -0500

    Modify signup form

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom'
 
 export default function SingUp() {
     return (
-        <div className="container mx-auto border h-full flex justify-center items-center">
-            <div className="bg-white w-1/3 h-96 rounded-lg shadow-lg p-8">
+        <div className="container mx-auto border h-full flex justify-center">
+            <div className="bg-white w-1/3 rounded-lg shadow-lg p-8 mt-28 h-auto mb-36">
                 <h1 className="text-4xl text-center font-normal text-indigo-800 ">Sign Up</h1>
                 <div>
                     <form className="form-horizontal w-3/4 mx-auto" >
+                        <div className="flex flex-col mt-8">
+                            <input type="text" className="flex-grow h-12 px-4 border rounded border-indigo-300 text-indigo-900" name="firstName" placeholder="First Name:" />
+                        </div>
+                        <div className="flex flex-col mt-8">
+                            <input type="text" className="flex-grow h-12 px-4 border rounded border-indigo-300 text-indigo-900" name="lastName" placeholder="Last Name:" />
+                        </div>
+                        <div className="flex flex-col mt-8">
+                            <input type="email" className="flex-grow h-12 px-4 border rounded border-indigo-300 text-indigo-900" name="email" placeholder="Email:" required />
+                        </div>
                         <div className="flex flex-col mt-8">
                             <input type="text" className="flex-grow h-12 px-4 border rounded border-indigo-300 text-indigo-900" name="username" placeholder="Username:" />
                         </div>
